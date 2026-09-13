8eca86fbe91b5a44e3427e8d5af277c3211dde10
Nirmalya Sarkar <bishnusarkar4321@gmail.com>
2026-09-13 07:42:09 +0530

    Add WorldLens configuration

---
+++ world-news-globe/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  NEWS_PROVIDER: 'demo',
+  NEWS_API_KEY: '',
+  AUTO_ROTATE: true,
+  NEWS_HEATMAP: true,
+  GLOBE_IMAGE: 'https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg',
+  GLOBE_NIGHT_IMAGE: 'https://unpkg.com/three-globe/example/img/earth-night.jpg',
+  BUMP_IMAGE: 'https://unpkg.com/three-globe/example/img/earth-topology.png',
+  FALLBACK_IMAGE: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&w=1000&q=80'
+};
