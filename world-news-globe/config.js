f6443c495fc7bbc03e2e3b4d3fb85b275b1c001f
Nirmalya Sarkar <bishnusarkar4321@gmail.com>
2026-09-23 20:24:19 +0530

    Enable automatic RSS news updates

--- world-news-globe/config.js
+++ world-news-globe/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
-  NEWS_PROVIDER: 'demo',
+  NEWS_PROVIDER: 'rss-github-actions',
   NEWS_API_KEY: '',
   AUTO_ROTATE: true,
   NEWS_HEATMAP: true,
+  NEWS_REFRESH_HOURS: 24,
   GLOBE_IMAGE: 'https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg',
   GLOBE_NIGHT_IMAGE: 'https://unpkg.com/three-globe/example/img/earth-night.jpg',
   BUMP_IMAGE: 'https://unpkg.com/three-globe/example/img/earth-topology.png',
